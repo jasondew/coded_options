a3bdf4c475dbb34d3c1758f7bee077de19dc5a0f
Jason Dew <jason.dew@gmail.com>
2010-07-12 12:47:14 -0400

    fixing this to work on the instance, not the class itself

--- lib/coded_options/base.rb
+++ lib/coded_options/base.rb
@@ -12,7 +12,7 @@ module CodedOptions
     Constants.const_set plural_name.upcase, values
     Constants.const_set "#{singular_name}_options".upcase, to_options(values)
 
-    instance_eval <<-EOT, __FILE__, (__LINE__+1)
+    class_eval <<-EOT, __FILE__, (__LINE__+1)
       def #{singular_name}                          # def state
         return unless #{singular_name}_id           #   return unless state_id
         Constants::#{plural_name.upcase}[#{singular_name}_id]  #   STATES[state_id]
