a7730f561fe2ac54fb539111ddc98e45c82a74f5
Jason Dew <jason.dew@gmail.com>
2010-10-10 21:13:56 -0400

    fixing typo for rails 3

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 if ActiveSupport.methods.include?("on_load")
   # rails 3
   ActiveSupport.on_load(:active_record) do
-    Base.send :extend, CodedOptions
+    ActiveRecord::Base.send(:extend, CodedOptions)
   end
 else
   # rails 2
