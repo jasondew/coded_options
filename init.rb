048b069adcc474c4c4d3f04695929e15f58d1a14
Jason Dew <jason.dew@gmail.com>
2010-10-13 21:15:21 -0400

    using rails 3 initializer

--- init.rb
+++
@@ -1,9 +0,0 @@
-if ActiveSupport.methods.include?("on_load")
-  # rails 3
-  ActiveSupport.on_load(:active_record) do
-    ActiveRecord::Base.send(:extend, CodedOptions)
-  end
-else
-  # rails 2
-  ActiveRecord::Base.send(:extend, CodedOptions)
-end
--- lib/coded_options.rb
+++ lib/coded_options.rb
@@ -1,1 +1,9 @@
 require "coded_options/base"
+
+module CodedOptions
+  class Railtie < Rails::Railtie
+    initializer "coded_options.initialize" do |app|
+      ActiveRecord::Base.send :extend, CodedOptions
+    end
+  end
+end
